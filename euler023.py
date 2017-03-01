8c86c8aeca4fa395038794b4b9c690fe72c65e23
Jonnie <jonnie@w3.fi>
2017-03-01 15:26:20 +0200

    mupd

---
+++ euler023.py
@@ -0,0 +1,43 @@
+'''
+A perfect number is a number for which the sum of its proper
+divisors is exactly equal to the number.
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, 
+which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper
+divisors is less than n and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16,
+the smallest number that can be written as the sum of
+two abundant numbers is 24. By mathematical analysis, it can be shown that
+all integers greater than 28123 can be written as the sum of two abundant
+numbers. However, this upper limit cannot be reduced any further by
+analysis even though it is known that the greatest number that cannot
+be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+'''
+
+def is_abundant(num):
+	if num < 12: return False
+	total = 1
+	root = int(num**0.5)
+	for i in range(2, root+1):
+		if num % i == 0:
+			total += i + num/i
+	if root*root==num: total -= root
+	if num < total:
+		return True
+	return False
+
+abundants = set()
+sum_of_not = 0
+
+for i in range(1, 28124):
+	if not any((i-a in abundants) for a in abundants):
+		sum_of_not += i
+	if is_abundant(i):
+		abundants.add(i)
+
+print("Sum of numbers that cannot be written with as sum of two abunants:")
+print(sum_of_not)
